efffa6c4e4a26e653eadbe669858f6f93282dd73
AubreyJabal18 <aubreyjabal12345@gmail.com>
2023-11-14 08:24:54 +0800

    update

---
+++ audio test.py
@@ -0,0 +1,7 @@
+from pydub import AudioSegment
+from pydub.playback import play
+
+sound = AudioSegment.from_file("recordings/eng/and_it_is.mp3", format="mp3")
+play(sound)
+sound = AudioSegment.from_file("recordings/eng/and_it_is.mp3", format="mp3")
+play(sound)
